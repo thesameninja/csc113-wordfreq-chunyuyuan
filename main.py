b626a9efba6c27f7332449915aa6d7e754605703
thesameninja <107506915+thesameninja@users.noreply.github.com>
2022-11-20 13:28:51 -0500

    Restructuring and Organizing
    Put the pdf files in a different folder, downloaded the .txt file into our project directory, started the code for the project.

---
+++ main.py
@@ -0,0 +1,21 @@
+import string
+import time
+import os
+import random
+
+#Code for reading the book.txt into python
+
+
+#function to remove the punctuations 
+def remove_punctuations():
+    pass
+
+
+#function to convert all the words into lowercase so we don't have same word being counted distinctly
+def covert_lowercase():
+    pass
+
+#we can use a dictionary to store the words, function to split it by each word.
+
+
+#Other team members can work here - Sohail>>>
