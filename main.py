30327d689a5c7d349380f12e57c917b461f4742b
thesameninja <107506915+thesameninja@users.noreply.github.com>
2022-11-27 16:14:15 -0500

    Implemented text manipulation functions
    *Added functions to read the book and manipulate the words, outputting the sorted list as a text file.

--- main.py
+++ main.py
@@ -3,19 +3,63 @@ import time
 import os
 import random
 
+
+#Useful links for you guys: 
+#https://www.geeksforgeeks.org/with-statement-in-python/
+#https://www.w3schools.com/python/python_dictionaries.asp
+#https://www.w3schools.com/python/python_tuples.asp
+
+#-Sohail Ahmad
+
+
 #Code for reading the book.txt into python
+filepath = "/home/np/GitHub/csc113-wordfreq-chunyuyuan/book.txt" #/home/np/GitHub/csc113-wordfreq-chunyuyuan/book.txt
+out_filepath = "/home/np/GitHub/csc113-wordfreq-chunyuyuan/output.txt"
+word_count = {}
 
 
 #function to remove the punctuations 
-def remove_punctuations():
-    pass
+def remove_punctuations(line):
+    for character in string.punctuation:
+        line = line.replace(character, "")
+    return line
 
 
-#function to convert all the words into lowercase so we don't have same word being counted distinctly
-def covert_lowercase():
-    pass
+#sorting the dictionary with tuple, then reversing the tuplef to get top to bottom.
+def ordered_dict_by_freq(dictionary):
+    sorted_values = []
+    for key in dictionary:
+        sorted_values.append((dictionary[key],key))
+    sorted_values = sorted(sorted_values)
 
-#we can use a dictionary to store the words, function to split it by each word.
+    sorted_values = sorted_values[::-1]
+    return sorted_values
 
 
-#Other team members can work here - Sohail>>>
+#using filepath to acces file, removing punctuations, 
+with open(filepath, 'r') as fi:
+    for line in fi:
+        line = remove_punctuations(line)
+        words = line.split()
+
+        for words in words:
+            words = words.lower()
+            if words not in word_count:
+                word_count[words] = 0
+            word_count[words] += 1
+
+#Output list of top words into output.txt
+#https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
+#refer to the stackoverflow for better understanding of the with block.
+
+top_words = ordered_dict_by_freq(word_count)
+for tuple_freq in top_words:
+    count, word = tuple_freq
+    with open("output.txt", "a") as f:
+        print("{0:20}{1:8d}".format(word, count),file=f)
+
+
+#Other team members can work here - Sohail>>> 
+#You guys can start adding your code, we need to figure out multi-threading/processing
+#implement a way to then measure the time taken for the program to run for each specification.
+#-Sohail Ahmad 11/27/2022
